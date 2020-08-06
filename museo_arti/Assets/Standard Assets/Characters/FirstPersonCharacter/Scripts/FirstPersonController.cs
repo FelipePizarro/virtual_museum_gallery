1da55ec3b6ddd2276e886c49a116ad0dab18bb63
Felipe Pizarro <52075875+FelipePizarro@users.noreply.github.com>
2020-08-06 16:15:27 +0200

    VM: rotate view on drag

--- museo_arti/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ museo_arti/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2b5eb4b17d69b6ed8cc17aed0332db711d739d50187b418dd7fa6e61ca4ae540
-size 10074
+oid sha256:3601f9373af70c63a658e81a7fff3e1e9552cf23c483a5607079de7b9a088d9d
+size 10533
