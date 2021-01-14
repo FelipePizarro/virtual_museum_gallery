068f815009e7af648b6abf4c1e4f259c01c87b1f
Felipe Pizarro <f.pizarroeltit@gmail.com>
2021-01-14 12:18:07 +0100

    update 14-01

--- museo_arti/Assets/Picture Scripts/PictureManager.cs
+++ museo_arti/Assets/Picture Scripts/PictureManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aade3e41b36c0afc586dbe1b4b79d6e1048abe4a5f5e610c2d8b206bf2bcc6ba
-size 5850
+oid sha256:666897ff8af8eb8d28fdfe5fb0fd000de0960cb3c1bb3e42d91da114a4d2cbe7
+size 5598
